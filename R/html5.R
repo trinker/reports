c53be6ac248b586b9222f1186ad83c3a212d6fff
tyler rinker <tyler_rinker@hotmail.com>
2013-02-24 02:08:10 -0500

    added a tex2docx function for conveinence

--- R/html5.R
+++ R/html5.R
@@ -8,14 +8,16 @@
 #' writes the original HTML file.  Default, \code{NULL}, uses the root name of 
 #' the \code{in.file} plus a number 2.
 #' @param ref.page The title of the reference page (adds hanging indent and 
-#' reduces font size).  If \code{NULL} references slide will not be adjusted.  If 
-#' reference title is not found a warning will print.
+#' reduces font size).  If \code{NULL} references slide will not be adjusted.  
+#' If reference title is not found a warning will print.
 #' @param refs.font.size The font size to make the references.
-#' @param path The path to where the project should be created.  Default is the 
-#' PRESENTATION directory.
+#' @param path The path to where the documents reside/should be created.  
+#' Default is the PRESENTATION directory.  This conveniently allows for non 
+#' paths to be spplied to \code{in.file} and \code{out.file}.  Paths can be 
+#' supplied to \code{in.file} and \code{out.file} by setting \code{path} to 
+#' \code{NULL}.
 #' @details The user must have pandoc installed and on their path.  pandoc can 
 #' be instaleld from: \cr \href{http://johnmacfarlane.net/pandoc/installing.html}{http://johnmacfarlane.net/pandoc/installing.html}
-#' @return Creates a report template.
 #' @export
 #' @examples 
 #' \dontrun{
@@ -25,17 +27,19 @@
 html5 <-
 function(in.file = NULL, out.file = NULL, ref.page = "References", 
         refs.font.size = 14, path = paste0(getwd(), "/PRESENTATION")) {
-    WD <- getwd()
-    on.exit(setwd(WD))
-    setwd(path)   
-    if (is.null(in.file)) {
-        in.file <- dir(path)[tools::file_ext(dir(path)) == "md"][1]
-    }
-    if (!is.null(out.file) && out.file=="replace") {
-        out.file <- paste0(unlist(strsplit(in.file, "\\."))[1], ".html")
-    }
-    if (is.null(out.file)) {
-        out.file <- paste0(unlist(strsplit(in.file, "\\."))[1], "2.html")
+    if (!is.null(path)) {
+        WD <- getwd()
+        on.exit(setwd(WD))
+        setwd(path)   
+        if (is.null(in.file)) {
+            in.file <- dir(path)[tools::file_ext(dir(path)) == "md"][1]
+        }
+        if (!is.null(out.file) && out.file=="replace") {
+            out.file <- paste0(unlist(strsplit(in.file, "\\."))[1], ".html")
+        }
+        if (is.null(out.file)) {
+            out.file <- paste0(unlist(strsplit(in.file, "\\."))[1], "2.html")
+        }
     }
     action <- paste0(wheresPandoc(), " -s -S -i -t dzslides --mathjax ", in.file, 
         " -o ", out.file)
---
+++ R/tex2docx.R
@@ -0,0 +1,44 @@
+#' Convert tex to docx 
+#' 
+#' Use \href{pandoc}{http://johnmacfarlane.net/pandoc/} to convert tex to docx 
+#' for those colleagues who use docx. 
+#' 
+#' @param in.file A character vector of the md file.
+#' @param out.file A character vector of the outfile.  If \code{"replace"} over 
+#' writes the original HTML file.  Default, \code{NULL}, uses the root name of 
+#' the \code{in.file} plus a number 2.
+#' @param path The path to where the documents reside/should be created.  
+#' Default is the REPORT directory.  This conveniently allows for non paths to 
+#' be spplied to \code{in.file} and \code{out.file}.  Paths can be supplied to 
+#' \code{in.file} and \code{out.file} by setting \code{path} to \code{NULL}.
+#' @param bib.loc Optional path to a .bib resource.
+#' @details The user must have pandoc installed and on their path.  pandoc can 
+#' be instaleld from: \cr \href{http://johnmacfarlane.net/pandoc/installing.html}{http://johnmacfarlane.net/pandoc/installing.html}
+#' @export
+#' @examples 
+#' \dontrun{
+#' DOC <- system.file("extdata/doc_library/apa6.qual_tex", 
+#'    package = "reports")
+#' tex2docx(DOC, "test.docx")  
+#' }
+tex2docx <-
+function(in.file = NULL, out.file = NULL, path = paste0(getwd(), "/REPORT"), 
+    bib.loc = getOption("bib.loc")) {
+    if (!is.null(path)) {
+        WD <- getwd()
+        on.exit(setwd(WD))
+        setwd(path)   
+        if (is.null(in.file)) {
+            in.file <- dir(path)[tools::file_ext(dir(path)) == "md"][1]
+        }
+        if (is.null(out.file)) {
+            out.file <- paste0(unlist(strsplit(in.file, "\\."))[1], ".docx")
+        }
+    }
+    action <- paste0(wheresPandoc(), " -s ", in.file, " -o ", out.file)
+    if (!is.null(bib.loc)) {
+        action <- paste0(action, " --bibliography=", bib.loc)
+    }
+    system(action)
+    cat("docx file generated!")
+}
