f2e05be06ccc271ec344d72eeb83cb4237456f1d
tyler rinker <tyler_rinker@hotmail.com>
2013-02-24 17:43:46 -0500

    added space

--- R/reports-package.R
+++ R/reports-package.R
@@ -1,7 +1,7 @@
 #' reports: Assists in the workflow of writing reports
 #'
 #' reports is a package that assists in writing apa6 style reports
-#' and presentations.  The package is designed to be used with RStudio,
+#' and presentations.  The package is designed to be used with RStudio, 
 #' MikTex/TexLive, knitr and Pandoc.  The user will want to download these 
 #' free programs to maximize the effectiveness of the reports package.
 #'
