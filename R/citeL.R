37343e50c55033815c13a2b9061f9233f945ffd3
tyler rinker <tyler_rinker@hotmail.com>
2013-03-23 15:58:31 -0400

    added possciteL and posciteL commands.  Latex citations are finished.  Now the Mark down versions (specify width and no page numbers)
    issue #19

--- R/citeL.R
+++ R/citeL.R
@@ -118,4 +118,96 @@ textciteL <- function(text.loc = NULL, copy2clip = TRUE, citation = TRUE) {
 	invisible(x)
 }
 
+#' @rdname cite 
+#' @export 
+possciteL <- function(text.loc = NULL, copy2clip = TRUE, citation = TRUE) {
+	out <- CITEhelper(text.loc = text.loc)
+    text <- out[[1]]
+    if (wc(text) > 39) {
+        LONG <- TRUE
+        L <- "\\begin{quote}\n"
+        R <- "\n\\end{quote}"
+    } else {
+    	LONG <- FALSE    	
+        L <- "``"
+        R <- "'' "
+    }
+	citeK <- NULL
+    if (citation) {
+    	PP <- grepl("-", out[[2]])
+    	PP <- ifelse(PP, "pp", "p")
+    	citeK <- paste("\\posscite{", out[[3]], "}", sep="")
+    	pgs <- paste("(", PP, ". ", out[[2]], ")", sep="")
+        if (!LONG) {
+            x <- paste(citeK, " ", L, text, R, pgs, sep="")	
+        } else {
+        	x <- paste(citeK, "\n", L, paste(text, pgs), R, collapse = "\n")
+        }    	
+    }
+    if(copy2clip){
+        if (Sys.info()["sysname"] == "Windows") {
+            writeClipboard(x, format = 1)
+        }
+        if (Sys.info()["sysname"] == "Darwin") {           
+            j <- pipe("pbcopy", "w")                       
+            writeLines(x, con = j)                               
+            close(j)                                    
+        }             
+    }
+    if (LONG) {
+    	bod <- strWrap(paste(text, pgs), copy2clip = FALSE, invisible=TRUE)
+    	body <- paste(paste(" ", bod), collapse="\n")
+        cat(citeK); cat("\n"); cat(L); cat(body); cat(R); cat("\n")
+    } else {
+        cat(x)
+    }
+	invisible(x)
+}
+
+#' @rdname cite 
+#' @export 
+posciteL <- function(text.loc = NULL, copy2clip = TRUE, citation = TRUE) {
+	out <- CITEhelper(text.loc = text.loc)
+    text <- out[[1]]
+    if (wc(text) > 39) {
+        LONG <- TRUE
+        L <- "\\begin{quote}\n"
+        R <- "\n\\end{quote}"
+    } else {
+    	LONG <- FALSE    	
+        L <- "``"
+        R <- "'' "
+    }
+	citeK <- NULL
+    if (citation) {
+    	PP <- grepl("-", out[[2]])
+    	PP <- ifelse(PP, "pp", "p")
+    	citeK <- paste("\\poscite{", out[[3]], "}", sep="")
+    	pgs <- paste("(", PP, ". ", out[[2]], ")", sep="")
+        if (!LONG) {
+            x <- paste(citeK, " ", L, text, R, pgs, sep="")	
+        } else {
+        	x <- paste(citeK, "\n", L, paste(text, pgs), R, collapse = "\n")
+        }    	
+    }
+    if(copy2clip){
+        if (Sys.info()["sysname"] == "Windows") {
+            writeClipboard(x, format = 1)
+        }
+        if (Sys.info()["sysname"] == "Darwin") {           
+            j <- pipe("pbcopy", "w")                       
+            writeLines(x, con = j)                               
+            close(j)                                    
+        }             
+    }
+    if (LONG) {
+    	bod <- strWrap(paste(text, pgs), copy2clip = FALSE, invisible=TRUE)
+    	body <- paste(paste(" ", bod), collapse="\n")
+        cat(citeK); cat("\n"); cat(L); cat(body); cat(R); cat("\n")
+    } else {
+        cat(x)
+    }
+	invisible(x)
+}
+
   
