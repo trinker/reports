470766ea5eb645588bf1e04db5d5cd1060f967a9
tyler rinker <tyler_rinker@hotmail.com>
2013-03-23 15:47:01 -0400

    added textciteL function issue #19
    No longer including H version citations and adding posscite and poscite

--- R/citeL.R
+++ R/citeL.R
@@ -1,6 +1,36 @@
+#' Format Quotes with Citations
+#' 
+#' Citation tools to grab a quote and format the text taken from the 
+#' notes.xlsx/notes.csv for LaTeX with optional citation included.  Functions 
+#' attempt to copy the output to the clipboard for easy paste inclusion.     
+#' 
+#' @param text.loc The row number (integer value) from notes.xlsx/notes.csv of 
+#' the text to insert.  The user may also input a character string of partial 
+#' matches of the bibkeys (a quasi-author search).  If NULL the interactive use 
+#' allows the user to view the notes.xlsx/notes.csv and chose a row number.
+#' @param copy2clip logical.  If \code{TRUE} attempts to copy the output to the 
+#' clipboard.
+#' @param citation logical.  If TRUE will include the formatted citation + the 
+#' quote.
+#' @details Each of the functions in the cite family follow a pattern of 
+#' (cite, textcite, posscite, poscite) prefix and (L or M) suffix.  The 
+#' cite and textcite are in the form of LaTeX commands by the same name.  
+#' posscite and poscite are user defined LaTeX function styles that are 
+#' extensions of the textcite command to fit possessive and -s- ending 
+#' possessives.  They can be defined as:
+#' 
+#' \code{\\newcommand\\posscite[1]{\\citeauthor{#1}'s (\\citeyear{#1})}} \cr
+#' \code{\\newcommand\\poscite[1]{\\citeauthor{#1}' (\\citeyear{#1})}} \cr
+#'
+#' The L and M correspond to LaTeX or markdown outputs; markdown relies on the 
+#' \code{knitcitations} package.
+#' @section Note: It is expected that the user maintains notes.xlsx/notes.csv 
+#' with markdown notation (e.g., ** for bold type and * for italics).
+#' @rdname cite
+#' @return Returns a character vector with LaTeX/markdown formatted text.
 #' @export
-citeL <- function(text = NULL, copy2clip = TRUE, citation = TRUE) {
-	out <- CITEhelper(text = text)
+citeL <- function(text.loc = NULL, copy2clip = TRUE, citation = TRUE) {
+	out <- CITEhelper(text.loc = text.loc)
     text <- out[[1]]
     if (wc(text) > 39) {
         LONG <- TRUE
@@ -36,6 +66,52 @@ citeL <- function(text = NULL, copy2clip = TRUE, citation = TRUE) {
     	bod <- strWrap(paste(text, citeK), copy2clip = FALSE, invisible=TRUE)
     	body <- paste(paste(" ", bod), collapse="\n")
         cat(L); cat(body); cat(R); cat("\n")
+    } else {
+        cat(x)
+    }
+	invisible(x)
+}
+
+#' @rdname cite 
+#' @export 
+textciteL <- function(text.loc = NULL, copy2clip = TRUE, citation = TRUE) {
+	out <- CITEhelper(text.loc = text.loc)
+    text <- out[[1]]
+    if (wc(text) > 39) {
+        LONG <- TRUE
+        L <- "\\begin{quote}\n"
+        R <- "\n\\end{quote}"
+    } else {
+    	LONG <- FALSE    	
+        L <- "``"
+        R <- "'' "
+    }
+	citeK <- NULL
+    if (citation) {
+    	PP <- grepl("-", out[[2]])
+    	PP <- ifelse(PP, "pp", "p")
+    	citeK <- paste("\\textcite{", out[[3]], "}", sep="")
+    	pgs <- paste("(", PP, ". ", out[[2]], ")", sep="")
+        if (!LONG) {
+            x <- paste(citeK, " ", L, text, R, pgs, sep="")	
+        } else {
+        	x <- paste(citeK, "\n", L, paste(text, pgs), R, collapse = "\n")
+        }    	
+    }
+    if(copy2clip){
+        if (Sys.info()["sysname"] == "Windows") {
+            writeClipboard(x, format = 1)
+        }
+        if (Sys.info()["sysname"] == "Darwin") {           
+            j <- pipe("pbcopy", "w")                       
+            writeLines(x, con = j)                               
+            close(j)                                    
+        }             
+    }
+    if (LONG) {
+    	bod <- strWrap(paste(text, pgs), copy2clip = FALSE, invisible=TRUE)
+    	body <- paste(paste(" ", bod), collapse="\n")
+        cat(citeK); cat("\n"); cat(L); cat(body); cat(R); cat("\n")
     } else {
         cat(x)
     }
