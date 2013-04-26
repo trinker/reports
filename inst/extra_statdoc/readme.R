e4190a124f348506bf3063dc1d72b3f3a1a34f6f
tyler rinker <tyler_rinker@hotmail.com>
2013-04-26 11:11:48 -0400

    updated

--- inst/extra_statdoc/readme.R
+++ inst/extra_statdoc/readme.R
@@ -1,4 +1,4 @@
-<p><img src="https://dl.dropboxusercontent.com/u/61803503/packages/reports.PNG" width="280" height="185"></p>
+<p><a href="http://trinker.github.io/reports/dependencies"><img src="https://dl.dropboxusercontent.com/u/61803503/packages/reports.PNG" width="280" height="185"></a></p>
 
 
 <p><a href="https://github.com/trinker/reports"> reports</a> is a package that assists in writing reports and presentations by providing a frame work that brings together existing R, LaTeX/.docx and Pandoc tools.  The package is designed to be used with <a href="http://www.rstudio.com/">RStudio</a>, <a href="http://miktex.org/">MikTex</a>/<a href="http://www.tug.org/texlive/">Tex Live</a>/<a href="http://www.libreoffice.org/">LibreOffice</a>, <a href="http://yihui.name/knitr/">knitr</a>, <a href="http://ramnathv.github.com/slidify/">slidify</a>, <a href="http://www.carlboettiger.info/2012/05/30/knitcitations.html">knitcitations</a>, <a href="http://johnmacfarlane.net/pandoc/">Pandoc</a> and <a href="https://github.com/rapporter/pander">pander</a>.  The user will want to download these free programs to maximize the effectiveness of the reports package.</p>
