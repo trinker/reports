335d07f14682b7895023d1ee6ff6d1cc48c37cb2
tyler rinker <tyler_rinker@hotmail.com>
2013-04-29 00:52:55 -0400

    added col2hex

--- R/col2hex.R
+++ R/col2hex.R
@@ -5,10 +5,15 @@
 #' @param rcolor A character string taken from R's built-in color names.
 #' @return Returns a character string of converted hexadecimal colors. 
 #' @export
+#' @seealso 
+#' \code{\link[grDevices]{colors}},
+#' \code{\link[grDevices]{rgb}},
+#' \code{\link[grDevices]{col2rgb}}
 #' @examples
 #' col2hex("darkblue")
 #' col2hex(c("darkblue", "red"))
-#' cat(paste0("<hr color=\"", col2hex("red"), "\" size=\"4\">"))
+#' x <- paste0("<hr color=\"", col2hex("red"), "\" size=\"4\">")
+#' cat(x)
 col2hex <- function(rcolor) {
     cols <- c(rgb(t(col2rgb(colors())), maxColorValue=255))
     colCheck <- colors() %in% rcolor
