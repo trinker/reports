e74457cf898d910fbfb296e0363c0c954268f448
tyler rinker <tyler_rinker@hotmail.com>
2013-04-28 23:34:30 -0400

    added new_win to HR

--- R/HR.R
+++ R/HR.R
@@ -2,11 +2,12 @@
 #' 
 #' Wrap a path/url to generate an HTML href tag.
 #' 
-#' @param text character vector of text to hyperref from.  Defualt uses the 
-#' \code{\link[base]{basename}} of the path.
 #' @param path character vector url/path copied to the clipboard. Default is to 
 #' read from the clipboard.  Note that Windows users do not have to reorient 
 #' slashes in local paths if reading from the clipboard.
+#' @param text character vector of text to hyperref from.  Defualt uses the 
+#' \code{\link[base]{basename}} of the path.
+#' @param new_win logical.  If \code{TRUE} the link will open in a new window.
 #' @param copy2clip logical.  If \code{TRUE} attempts to copy the output to the 
 #' clipboard.  
 #' @param print logical.  If TRUE \code{\link[base]{cat}} prints the output to the 
@@ -19,7 +20,7 @@
 #' HR(path="http://dl.dropbox.com/u/61803503/Likert.pdf", print = TRUE)
 #' HR("http://cran.r-project.org/src/contrib/reports_0.1.2.tar.gz", print = TRUE)
 #' HR("http://cran.r-project.org/src/contrib/reports_0.1.2.tar.gz", "click me", print = TRUE)
-HR <- function(path = "clipboard", text = NULL, copy2clip = TRUE, 
+HR <- function(path = "clipboard", text = NULL, new_win = TRUE, copy2clip = TRUE, 
     print = FALSE) {
     if (path == "clipboard") {
         path <- read_clip()
@@ -28,7 +29,12 @@ HR <- function(path = "clipboard", text = NULL, copy2clip = TRUE,
     if (is.null(text)) {
         text <- basename(path)
     }
-    x <- paste0("<a href=\"", path, "\">", text, "</a>\n")
+    if (new_win) {
+        tar <- "target=\"_blank\""	
+    } else {
+    	tar <- NULL
+    }
+    x <- paste0("<a href=\"", path, "\"", tar, ">", text, "</a>\n")
     if(copy2clip){
         write_clip(x)
     }
