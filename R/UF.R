1bc37fcd90a3e92705486407636828adbacedc2a
tyler rinker <tyler_rinker@hotmail.com>
2013-05-11 21:41:26 -0400

    updated documentation

--- R/UF.R
+++ R/UF.R
@@ -1,6 +1,6 @@
 #' Path/URL: Fill Blank Spaces
 #' 
-#' \code{UF} - A convenience wrapper to replace black spaces of paths with 
+#' \code{UF} - A convenience function to replace black spaces of paths with 
 #' underscores. 
 #' 
 #' @param path A character vector or url/path copied to the clipboard.  Default 
@@ -9,7 +9,8 @@
 #' clipboard.
 #' @param fill The character symbol to fill the blank spaces with.
 #' @return Returns a character vector every space replaced with an 
-#' underscore/percent.  \code{UF} also converts the text to lower case.
+#' underscore/percent And apostophes removed.  \code{UF} also converts the text 
+#' to lower case.
 #' @section Warning: Ligatures parsing is very good, however, these elements my
 #' be incorrect.  If a warning is thrown check the use of "ff", "fi", "fl", 
 #' "ffi" and "ffl".
@@ -24,7 +25,7 @@ UF <- function(path = "clipboard", copy2clip = TRUE, fill = "_"){
     } 
     text <- text_fix(path)
     und <- function(x) { 
-        gsub("\\s+", fill, x)
+        gsub("'", "", gsub("\\s+", fill, x))
     } 
     x <- unlist(lapply(tolower(text), und)) 
     if(copy2clip){
@@ -35,7 +36,7 @@ UF <- function(path = "clipboard", copy2clip = TRUE, fill = "_"){
 
 #' Path/URL: Fill Blank Spaces
 #'
-#' \code{PF} - A convenience wrapper to replace black spaces of URLs with 
+#' \code{PF} - A convenience function to replace black spaces of URLs with 
 #' percents.
 #' 
 #' @export
@@ -46,7 +47,7 @@ PF <- function(path = "clipboard", copy2clip = TRUE, fill = "%"){
     } 
     text <- text_fix(path)
     und <- function(x) { 
-        gsub("\\s+", fill, x)
+        gsub("'", "", gsub("\\s+", fill, x))
     } 
     x <- unlist(lapply(text, und)) 
     if(copy2clip){
