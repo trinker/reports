d7eb87986ebfbda9b57161b43ec7551a720241d2
tyler rinker <tyler_rinker@hotmail.com>
2013-05-21 01:41:18 -0400

    updated documentation

---
+++ R/HL.R
@@ -0,0 +1,35 @@
+#' Highlight HTML Text
+#' 
+#' Wraps text with a background color specific font tags. 
+#'
+#' @param text A character vector or text copied to the clipboard.  Default is to 
+#' read from the clipboard.
+#' @param color A character string taken from R's built-in color names or a 
+#' hexidecimal color.
+#' @param copy2clip logical.  If \code{TRUE} attempts to copy the output to the 
+#' clipboard. 
+#' @param print logical.  If TRUE \code{\link[base]{cat}} prints the output to the 
+#' console.  If \code{FALSE} returns to the console.
+#' @export
+#' @examples
+#' cat(HL("Do not trust robots!"), "They are bent on destruction.")
+#' cat(HL("Jake is a cookie scientist,", color="pink"), "an honrable profession.")
+HL <-
+function(text = "clipboard", color = "yellow", copy2clip = TRUE, print = FALSE) { 
+    if (text == "clipboard") {
+        text <- read_clip()
+    }
+    a <- "<font style=\"background-color: "
+    if (!grepl("#", color)) {
+        color <- col2hex(color)
+    }
+    b <- ";\">"
+    d <- "</font>"
+    x <- paste0(a, color, b, text, d)
+
+    if(copy2clip){
+        write_clip(x)
+    }
+    prin(x = x, print = print)
+}
+
--- R/PN.R
+++ R/PN.R
@@ -1,6 +1,6 @@
 #' Insert Presenter Notes
 #' 
-#' Creates an aside of the class "notes" for reveal.js slides
+#' Creates an aside of the class "notes" for slides (e.g., reveal.js slides).
 #' 
 #' @param text A character vector or text copied to the clipboard.  Default is 
 #' to read from the clipboard.
@@ -12,7 +12,7 @@
 #' @export
 #' @examples
 #' PN("some fancy notes", print = TRUE)
-#' PN("1) some\n2) fancy\n3) notes")
+#' PN("1) some\n2) fancy\n3) notes", print = TRUE)
 #' PN("1) some 
 #' 2) fancy 
 #' 3) notes", print = TRUE)
