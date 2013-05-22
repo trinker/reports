53f23ab422f58b6f5066f5c8d8d66efd7c2790ef
tyler rinker <tyler_rinker@hotmail.com>
2013-05-22 11:57:42 -0400

    update

--- inst/extdata/doc_library/wordpress_web/inst/SEND_IT.R
+++
@@ -1,5 +0,0 @@
-options(WordpressLogin = c(USERNAME = "PASSWORD"), WordpressURL = "http://INSERT_WP_URL_HERE/xmlrpc.php")
-# install.packages("RWordPress", repos = "http://www.omegahat.org/R", type = "source")
-
-library(pacman); p_load(knitr, RWordPress)
-knit2wp("yourfile.Rmd", title = "TITLE")
