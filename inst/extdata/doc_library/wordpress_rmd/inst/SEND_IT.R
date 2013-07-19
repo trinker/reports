ec2e9d7e1bea86a6a19db545b6a7bb45fef78c8b
tyler rinker <tyler_rinker@hotmail.com>
2013-07-18 20:24:01 -0400

    updated templates

--- inst/extdata/doc_library/wordpress_rmd/inst/SEND_IT.R
+++ inst/extdata/doc_library/wordpress_rmd/inst/SEND_IT.R
@@ -1,5 +1,8 @@
+## Fill in anything in capital letters
+
 options(WordpressLogin = c(USERNAME = "PASSWORD"), WordpressURL = "http://INSERT_WP_URL_HERE/xmlrpc.php")
 # install.packages("RWordPress", repos = "http://www.omegahat.org/R", type = "source")
 
 library(pacman); p_load(knitr, RWordPress)
-knit2wp("yourfile.Rmd", title = "TITLE")
+knit2wp("YOUR_RMD_FILE_NAME.Rmd", title = "BLOG TITLE")
+
