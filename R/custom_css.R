54e2786d758a69770ecfdc99466bf11c92ac60f0
tyler rinker <tyler_rinker@hotmail.com>
2013-08-02 13:33:14 -0400

    add styl.css argument to custom_css

--- R/custom_css.R
+++ R/custom_css.R
@@ -4,14 +4,17 @@
 #' knitr.
 #'
 #' @param loc Path to the report location where the custom css should be placed.
+#' @param style.css An optional path to a style.css file that will be used as 
+#' the ~/css/style.css.
 #' @details The user must add the custom contents to the custom css located in  
 #' ~/css/style.css
 #' @note The user has to source before the custom styles will be applied. Open 
-#' the style.R as the active tab in RStudio, and check the `Source on Save` box.
+#' the style.R as the active tab in RStudio, check the `Source on Save` box and 
+#' the click the save icon.
 #' @references
 #' \url{http://nsaunders.wordpress.com/2012/08/27/custom-css-for-html-generated-using-rstudio/} 
 #' @export
-custom_css <- function(loc = file.path(getwd(), "REPORT")) {
+custom_css <- function(loc = file.path(getwd(), "REPORT"), style.css = NULL) {
 	
 	## Check css directory existence
     if (!file.exists(file.path(loc, "css"))) {
@@ -22,9 +25,14 @@ custom_css <- function(loc = file.path(getwd(), "REPORT")) {
             "already exists.\n Directory `css` not generated"))	
     }
     
-    ## check style.css existence
+    ## check style.css existence/create style.css
     if (!file.exists(file.path(loc, "css", "style.css"))) {
-        cat("", file=file.path(cssloc, "style.css"))
+    	if (is.null(style.css)) {
+            cat("", file=file.path(cssloc, "style.css"))
+    	} else {
+    		sty <- suppressWarnings(readLines(style.css))
+            cat(paste(sty, collapse="\n"), file=file.path(cssloc, "style.css"))
+    	}
     }  else {
         warning(paste(file.path(loc, "css", "style.css"), 
             "already exists.\n File `~css/style.css` not generated"))	
@@ -34,8 +42,7 @@ custom_css <- function(loc = file.path(getwd(), "REPORT")) {
     x <- c("options(rstudio.markdownToHTML =", 
         "  function(inputFile, outputFile) {",      
         "    require(markdown)",
-        paste0("    markdownToHTML(inputFile, outputFile, stylesheet=\"", 
-            file.path(cssloc, "style.css"), "\")"),
+        "    markdownToHTML(inputFile, outputFile, stylesheet=file.path(getwd(), \"css/style.css\"))",
         "  }",
         ")"
     )
